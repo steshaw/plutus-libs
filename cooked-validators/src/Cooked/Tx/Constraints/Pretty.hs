baa52db52c798fb035cee924e2c02efc27a74565
Florent C <florentc@users.noreply.github.com>
2022-09-21 11:19:12 +0200

    Implement pretty-print for time constraints (#159)

--- cooked-validators/src/Cooked/Tx/Constraints/Pretty.hs
+++ cooked-validators/src/Cooked/Tx/Constraints/Pretty.hs
@@ -74,8 +74,9 @@ prettyMiscConstraint (SpendsScript val red spOut) =
     ("SpendsScript" <+> prettyTypedValidator val)
     "-"
     ["Redeemer:" <+> PP.viaShow red, prettyScriptOutputDatum val spOut]
-prettyMiscConstraint (ValidateIn r) = prettyEnum "ValidateIn" "-" $ [PP.pretty (show r)]
-prettyMiscConstraint _ = "<constraint without pretty def>"
+prettyMiscConstraint (Before time) = "Before:" <+> PP.pretty time
+prettyMiscConstraint (After time) = "After:" <+> PP.pretty time
+prettyMiscConstraint (ValidateIn timeRange) = "ValidateIn:" <+> PP.pretty timeRange
 
 prettyHash :: (Show a) => a -> Doc ann
 prettyHash = PP.pretty . take 6 . show
